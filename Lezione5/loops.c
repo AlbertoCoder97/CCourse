22e0e154e53e508684416ccddd20d8e9eac15d95
AlbertoCoder97 <alberto.calabrese.0297@gmail.com>
2023-01-10 20:08:09 +0100

    Added for loop example

---
+++ Lezione5/loops.c
@@ -0,0 +1,38 @@
+#include "loops.h"
+
+int main()
+{
+    whileLoop();
+    return 0;
+}
+
+/* The most common loop type is the for loop. 
+A loop is made by writing:
+for(index; expression; update) { do stuff; }
+Where the index is the element that gets indexed like int i = 0
+then the expression is evaluated at every iteration, if it's true
+it goes inside the for loop body and then the update
+is where you usually increment/decrease the variable. */
+void forLoop()
+{
+    for(int i = 0; i < 10; i++)
+    {
+        printf("For loop: %d\n", i);
+    }
+}
+
+/* Anything inside the while loop 
+is executed everytime the expression is true.
+If it's not it exits.
+You can have a break like in the switches
+to break out of the loop.*/
+void whileLoop()
+{
+    int x = 0;
+    while(1)
+    {
+        printf("I'm an endless loop.\n");
+        x++;
+        if(x > 10) break;
+    }
+}
---
+++ Lezione5/loops.h
@@ -0,0 +1,4 @@
+#include <stdio.h>
+
+void forLoop();
+void whileLoop();
