d9b4fbaefbc0236b913bcb8e13d224fea2e91eee
AlbertoCoder97 <alberto.calabrese.0297@gmail.com>
2023-01-15 11:24:59 +0100

    Added iofunctions

--- Lesson1/main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-void main(void)
-{
-    int number = 0;
-    printf("Input a number: ");
-    scanf("%d", &number);
-
-    printf("You inserted: %d\n", number);
-}
---
+++ Lezione5/iofunctions.c
@@ -0,0 +1,19 @@
+#include "iofunctions.h"
+
+/* Useful I/O Functions*/
+/* Terminal Only functions:
+* getchar() is an input function
+* putchar() is an output function */
+
+/* Array Stream functions:
+* getc()/fgetc() are input functions
+* putc()/fputc() are output functions
+* ungetc() is a particular function that if the read character is not needed pushes it back
+* and reads it again if you read chars from a stream again. You can use it to get out of loops
+* if your tokens contain unexpected chars. */
+
+int main()
+{
+    printf("WTF\n");
+    return 0;
+}
