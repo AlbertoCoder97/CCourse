91fca4836e9efab6efd0edbd4cfb3c5393b512e0
AlbertoCoder97 <alberto.calabrese.0297@gmail.com>
2023-01-16 17:02:42 +0100

    Added more memory notes.

--- Lezione6/memory.c
+++ Lezione6/memory.c
@@ -9,16 +9,34 @@
 * languages. If you allocate 1MB of memory, you need to clean the 1MB memory. */
 
 /* There are multiple functions that allow us to allocate memory: 
-* 1 - malloc() that initializes the memory big enough to store our object. 
-* 2 - calloc() is quite similar to malloc()
+* 1 - malloc() reserves the memory big enough to store our object. 
+* 2 - calloc() is quite similar to malloc() but calloc ALSO INITIALIZES THE MEMORY.
 * 3 - realloc() reallocates memory when buffer is small/too big.
 * 4 - free - THIS IS EXTREMELY IMPORTANT. It frees the memory allocated with malloc().
 * It's needed to free memory areas allocated with malloc to avoid filling memory/memory leaks.*/
 
+//When using malloc you need to cast the type in order to correctly indicate the type of
+//what malloc returns: pointer = (type*) malloc();
+
+/* malloc() is a library function in C, not a syscall. You should check malloc's return value.
+* When malloc fails, it returns NULL. */
+
+/* After using free you STILL have the pointer but the area might be used for something else.
+* Tip: after calling free(ptr) use ptr = NULL in this way you null the pointer. */
+
+/* After free you can reuse malloc to reallocate the memory freed since freed memory still
+* "belongs" to the process if it's still alive. */
+
+/* TIP: Instead of using calloc() use this:
+* p = malloc(SIZE)
+* bzero(p, SIZE) where bzero is a function that sets 0 in the memory designated initializing
+* the memory. */
+
+//ALWAYS CLEAR POINTERS
 
 int main()
 {
-    /* A - Static Memory Allocation*/
+    /* A - Static Memory Allocation */
     int x = 0;
     int array[3] = 0;
 
