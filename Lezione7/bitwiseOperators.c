4ff8bcee552d6489bdc982d8fe2c87f9fec009ef
AlbertoCoder97 <alberto.calabrese.0297@gmail.com>
2023-01-17 12:55:06 +0100

    Added bitwise operators notes.

---
+++ Lezione7/bitwiseOperators.c
@@ -0,0 +1,16 @@
+/* Other than arithmetical operators, in C there are bitwise operators that allow us to execute 
+* math functions dealing with bits. They work mostly with unsigned ints. Bitwise operators are: 
+* - and = &
+* - inclusive or = | 
+* - exclusive ore = ^ 
+* - complement = ~ 
+To not be confused and and or with && and ||. Single ones are bitwise operators and double ones are
+* boolean operators. 
+
+* There are also other types of operators called Shift operators. They are either Right-Shift or
+* Left-Shift. Shifting a number right of one position means that you move all the bits on the right and
+insert a 0 as the last bit. Same with left but to the left. Shift operators are:
+* - Right-Shift = >> 
+* - Left-Shift = << 
+* A Right shift of n position equals to a division by powers of 2. Shift of one = division of 2^1 etc...
+* Same with the left shift. */
---
+++ Lezione7/files.c
@@ -0,0 +1,4 @@
+/* There are multiple functions to read/write from/to a file. 
+* - fread(inArea, sizeof (int), 3, spData) reads a block of bytes from the file and stores it in memory. 
+* - fwrite() is the same but for writing from memory to disk. 
+*/
