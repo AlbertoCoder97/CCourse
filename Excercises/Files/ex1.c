fcd9e85f54934660f8a2e6bc049c93113e157b83
AlbertoCoder97 <alberto.calabrese.0297@gmail.com>
2023-01-19 10:48:06 +0100

    Added excercises

---
+++ Excercises/Files/ex1.c
@@ -0,0 +1,11 @@
+/* Write a program in C to create and store information in a text file 
+* Input a sentence for the file : This is the content of the file test.txt.
+* Expected Output: The file test.txt created successfully...!! 
+*/
+
+#include "ex1.h"
+
+void main(int argc, char *argv[])
+{
+    exit(0);
+}
